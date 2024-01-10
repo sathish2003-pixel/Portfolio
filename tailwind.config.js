c7021bb5c60385ce9caaca4b6a18f04455d3f4d1
sathish2003-pixel <satheshsathish2206@gmail.com>
2024-01-10 14:08:21 +0530

    portfolio created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 module.exports = {
   content: ["./main/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 }
 
+
+  
